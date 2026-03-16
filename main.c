a16e4761f24ff48264a3aa45d4bdb346cea85ffe
earthshipmax-lin <earthshipmax@gmail.com>
2026-03-16 16:23:07 +0800

    add selection sort

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include "bubble_sort.h"
+#include "selection_sort.h"
 
 int main()
 {
     int arr[5] = {5,4,3,2,1};
     int i;
 
-    bubbleSort(arr,5);
+    selectionSort(arr,5);
 
     for(i=0;i<5;i++)
     {
         printf("%d ",arr[i]);
     }
+
+    printf("\n");
 
     return 0;
 }
