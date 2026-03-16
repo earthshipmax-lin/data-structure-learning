acbec1caad1a4108eb66aff7e03487fb82003c9c
earthshipmax-lin <earthshipmax@gmail.com>
2026-03-16 16:58:51 +0800

    add insertion sort

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "selection_sort.h"
+#include "insertion_sort.h"
 
 int main()
 {
     int arr[5] = {5,4,3,2,1};
     int i;
 
-    selectionSort(arr,5);
+    insertionSort(arr,5);
 
     for(i=0;i<5;i++)
     {
