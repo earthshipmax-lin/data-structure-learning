01adb725590fb0195b27a0b6300c249b895679d7
earthshipmax-lin <earthshipmax@gmail.com>
2026-03-16 17:33:02 +0800

    add quick sort

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "insertion_sort.h"
+#include "quick_sort.h"
 
 int main()
 {
     int arr[5] = {5,4,3,2,1};
     int i;
 
-    insertionSort(arr,5);
+    quickSort(arr,0,4);
 
     for(i=0;i<5;i++)
     {
