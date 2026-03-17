1320d7c10a468b604578bd4e3bcb9b6854226a19
earthshipmax-lin <earthshipmax@gmail.com>
2026-03-17 18:22:15 +0800

    add merge sort

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "quick_sort.h"
+#include "merge_sort.h"
 
 int main()
 {
     int arr[5] = {5,4,3,2,1};
     int i;
 
-    quickSort(arr,0,4);
+    mergeSort(arr,0,4);
 
     for(i=0;i<5;i++)
     {
